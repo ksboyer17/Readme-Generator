fed2ab1e66998ad8ed8fefcf7dc45cf4a8f9866f
Kenny Boyer <ksboyerii@gmail.com>
2021-03-31 17:36:19 -0500

    created variables and began question statments

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,25 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
 
+// TODO: Include packages needed for this application
+//Done
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        name:'github',
+        message:'What is your github username',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
